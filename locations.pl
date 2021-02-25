8661ee6db7403df4c6402864adb165dc7f22d7d8
Guy Dobson <gdobson@drew.edu>
2021-02-25 14:57:40 -0500

    Create locations.pl
    also includes locinsts, loccamps, loclibs, & servicepoints

---
+++ locations.pl
@@ -0,0 +1,199 @@
+#!/usr/bin/perl
+
+require 'getExportImportTokens.pl';
+
+use JSON;
+
+$locinsts = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/location-units/institutions?query=id="*"`;
+$hash = decode_json $locinsts;
+for ( @{$hash->{locinsts}} ) {
+	$code = $_->{'code'};
+	$institutionId = $_->{'id'};
+	$name = $_->{'name'};
+	push(@locinstsData,"$name|$code");
+	$locinstsCodes{$institutionId} = $code;
+}
+
+$loccamps = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/location-units/campuses?query=id="*"`;
+$hash = decode_json $loccamps;
+for ( @{$hash->{loccamps}} ) {
+	$campusId = $_->{'id'};
+	$code = $_->{'code'};
+	$institutionId = $_->{'institutionId'};
+	$name = $_->{'name'};
+	$locinstsCode = $locinstsCodes{$institutionId};
+	push(@loccampsData,"$name|$code|$locinstsCode");
+	$loccampsCodes{$campusId} = $code;
+}
+
+$loclibs = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/location-units/libraries?query=id="*"`;
+$hash = decode_json $loclibs;
+for ( @{$hash->{loclibs}} ) {
+	$campusId = $_->{'campusId'};
+	$code = $_->{'code'};
+	$libraryId = $_->{'id'};
+	$name = $_->{'name'};
+	$loccampsCode = $loccampsCodes{$campusId};
+	push(@loclibsData,"$name|$code|$loccampsCode");
+	$loclibsCodes{$libraryId} = $code;
+}
+
+$servicepoints = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/service-points?query=id="*"`;
+$hash = decode_json $servicepoints;
+for ( @{$hash->{servicepoints}} ) {
+	$code = $_->{'code'};
+	$discoveryDisplayName = $_->{'discoveryDisplayName'};
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	push(@servicepointsData,"$name|$code|$discoveryDisplayName");
+	$servicepointsCodes{$id} = $code;
+}
+
+$locations = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/locations?query=id="*"`;
+$hash = decode_json $locations;
+for ( @{$hash->{locations}} ) {
+	$campusId = $_->{'campusId'};
+	$code = $_->{'code'};
+	$discoveryDisplayName = $_->{'discoveryDisplayName'};
+	$institutionId = $_->{'institutionId'};
+	$libraryId = $_->{'libraryId'};
+	$loccampsCode = $loccampsCodes{$campusId};
+	$locinstsCode = $locinstsCodes{$institutionId};
+	$loclibsCode = $loclibsCodes{$libraryId};
+	$name = $_->{'name'};
+	$primaryServicePoint = $_->{'primaryServicePoint'};
+	$primaryServicePointCode = $servicepointsCodes{$primaryServicePoint};
+	$servicePointCodes = "";
+        $servicePointIds = $_->{'servicePointIds'};
+        for ( @{$servicePointIds} ) {
+		$servicePointCodes .= $servicepointsCodes{$_} . "|";
+	}
+	$servicePointCodes =~ s/\|$//;
+	push(@locationsData,"$name|$code|$discoveryDisplayName|$loclibsCode|$loccampsCode|$locinstsCode|$primaryServicePointCode|$servicePointCodes");
+}
+
+$locations = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/locations?query=id="*"`;
+$hash = decode_json $locations;
+for ( @{$hash->{locations}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	$deleteLocations = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/locations/$id`; 
+	print "delete $name $deleteLocations \n\n";
+}
+
+$servicepoints = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/service-points?query=id="*"`;
+$hash = decode_json $servicepoints;
+for ( @{$hash->{servicepoints}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	$deleteServicepoints = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/service-points/$id`; 
+	print "delete $name $deleteServicepoints \n\n";
+}
+
+$loclibs = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/location-units/libraries?query=id="*"`;
+$hash = decode_json $loclibs;
+for ( @{$hash->{loclibs}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	$deleteLoclibs = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/location-units/libraries/$id`; 
+	print "delete $name $deleteLoclibs \n\n";
+}
+
+$loccamps = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/location-units/campuses?query=id="*"`;
+$hash = decode_json $loccamps;
+for ( @{$hash->{loccamps}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	$deleteLoccamps = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/location-units/campuses/$id`; 
+	print "delete $name $deleteLoccamps \n\n";
+}
+
+$locinsts = `curl -s -w '\n' -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/location-units/institutions?query=id="*"`;
+$hash = decode_json $locinsts;
+for ( @{$hash->{locinsts}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	$deleteLocinsts = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/location-units/institutions/$id`; 
+	print "delete $name $deleteLocinsts \n\n";
+}
+
+foreach $institution (@locinstsData) {
+	($name,$code) = split(/\|/,$institution);
+	$json = qq[{"name":"$name","code":"$code"}];
+	$postLocinsts = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/location-units/institutions`;
+	print "post $name $postLocinsts \n\n";
+	$getLocinsts = `curl -s -X GET -H '$jsonHeader' -H '$importToken' $importURL/location-units/institutions?query=code="$code"`;
+	$hash = decode_json $getLocinsts;
+	for ( @{$hash->{locinsts}} ) {
+		$locinstsIds{$code} = $_->{'id'};
+	}
+}
+
+foreach $campus (@loccampsData) {
+	($name,$code,$locinstsCode) = split(/\|/,$campus);
+	$institutionId = $locinstsIds{$locinstsCode};
+	$json = qq[{"name":"$name","code":"$code","institutionId":"$institutionId"}];
+	$postLoccamps = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/location-units/campuses`;
+	print "post $name $postLoccamps \n\n";
+	$getLoccamps = `curl -s -X GET -H '$jsonHeader' -H '$importToken' $importURL/location-units/campuses?query=code="$code"`;
+	$hash = decode_json $getLoccamps;
+	for ( @{$hash->{loccamps}} ) {
+		$loccampsIds{$code} = $_->{'id'};
+	}
+}
+
+foreach $library (@loclibsData) {
+	($name,$code,$loccampsCode) = split(/\|/,$library);
+	$campusId = $loccampsIds{$loccampsCode};
+	$json = qq[{"name":"$name","code":"$code","campusId":"$campusId"}];
+	$postLoclibs = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/location-units/libraries`;
+	print "post $name $postLoclibs \n\n";
+	$getLoclibs = `curl -s -X GET -H '$jsonHeader' -H '$importToken' $importURL/location-units/libraries?query=code="$code"`;
+	$hash = decode_json $getLoclibs;
+	for ( @{$hash->{loclibs}} ) {
+		$loclibsIds{$code} = $_->{'id'};
+	}
+}
+
+foreach $servicepoint (@servicepointsData) {
+	($name,$code,$discoveryDisplayName) = split(/\|/,$servicepoint);
+	$json = qq[{"name":"$name","code":"$code","discoveryDisplayName":"$discoveryDisplayName"}];
+	$postServicepoints = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/service-points`;
+	print "post $name $postServicepoints \n\n";
+	$getServicepoints = `curl -s -X GET -H '$jsonHeader' -H '$importToken' $importURL/service-points?query=code="$code"`;
+	$hash = decode_json $getServicepoints;
+	for ( @{$hash->{servicepoints}} ) {
+		$servicepointsIds{$code} = $_->{'id'};
+	}
+}
+
+foreach $location (@locationsData) {
+	@locationInfo = split(/\|/,$location);
+	$name = $locationInfo[0];
+	$code = $locationInfo[1];
+	$discoveryDisplayName = $locationInfo[2];
+	$loclibsCode = $locationInfo[3]; $libraryId = $loclibsIds{$loclibsCode};
+	$loccampsCode = $locationInfo[4]; $campusId = $loccampsIds{$loccampsCode};
+	$locinstsCode = $locationInfo[5]; $institutionId = $locinstsIds{$locinstsCode};
+	$primaryServicePointCode = $locationInfo[6]; $primaryServicePointId = $servicepointsIds{$primaryServicePointCode};
+	$servicePointIds = "";
+	for ($x = 7; $x <= $#locationInfo; $x++) {
+		if (length($locationInfo[$x]) > 0) {
+			$servicePointIds = qq["] . $servicepointsIds{$locationInfo[$x]} . qq[",];
+		}
+	}
+	chop($servicePointIds);
+	$json = qq/{
+	"name":"$name",
+	"code":"$code",
+	"isActive":true,
+	"discoveryDisplayName":"$discoveryDisplayName",
+	"institutionId":"$institutionId",
+	"campusId":"$campusId",
+	"libraryId":"$libraryId",
+	"primaryServicePoint":"$primaryServicePointId",
+	"servicePointIds":[$servicePointIds]
+	}/;
+	$postLocations = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/locations`;
+	print "post $name $postLocations\n\n";
+}
