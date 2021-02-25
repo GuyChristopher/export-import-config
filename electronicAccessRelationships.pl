b7d62f8bb42095fb711d9b695f54f8a3cb9997dc
Guy Dobson <gdobson@drew.edu>
2021-02-25 15:56:13 -0500

    aka URL relationship

---
+++ electronicAccessRelationships.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+require 'getExportImportTokens.pl'; 
+
+use JSON; 
+
+print "exporting table data \n";
+
+$electronicAccessRelationships = `curl -s -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/electronic-access-relationships?query=id="*"`;
+$hash = decode_json $electronicAccessRelationships;
+for ( @{$hash->{electronicAccessRelationships}} ) {
+	$name = $_->{'name'};
+	$source = $_->{'source'};
+	push(@tableData,"$name|$source");
+}
+print "@tableData \n\n";
+
+print "deleteing data from the import env \n";
+$electronicAccessRelationships = `curl -s -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/electronic-access-relationships?query=id="*"`;
+$hash = decode_json $electronicAccessRelationships;
+for ( @{$hash->{electronicAccessRelationships}} ) {
+	$id = $_->{'id'};
+	$name = $_->{'name'};
+	print "deleting $name \n";
+	$delete = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/electronic-access-relationships/$id`;
+	print "$delete\n";
+}
+
+print "\nimporting table data \n\n";
+foreach $row (@tableData) {
+	($name,$source) = split(/\|/,$row);
+	$json = qq[{"name":"$name","source":"$source"}];
+	$post = `curl -s -w '\n' -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/electronic-access-relationships`;
+	print "$post \n\n";
+}
+
