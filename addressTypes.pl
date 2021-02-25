12b7a77a5ccc194b49c68e97a9ba9168e44a9f1d
Guy Dobson <gdobson@drew.edu>
2021-02-25 17:05:07 -0500

    Create addressTypes.pl

---
+++ addressTypes.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+require 'getExportImportTokens.pl'; 
+
+use JSON; 
+
+print "exporting table data \n";
+$addressTypes = `curl -s -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/addresstypes?query=id="*"`;
+$hash = decode_json $addressTypes;
+for ( @{$hash->{addressTypes}} ) {
+	$addressType = $_->{'addressType'};
+	$desc = $_->{'desc'};
+	push(@tableData,"$addressType|$desc");
+}
+print "@tableData \n\n";
+
+print "deleteing data from the import env \n";
+$addressTypes = `curl -s -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit=1000' $importURL/addresstypes?query=id="*"`;
+$hash = decode_json $addressTypes;
+for ( @{$hash->{addressTypes}} ) {
+	$id = $_->{'id'};
+	$addressType = $_->{'addressType'};
+	print "deleting $addressType \n";
+	$delete = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/addresstypes/$id`;
+	print "$delete\n";
+}
+
+print "\nimporting table data \n\n";
+foreach $row (@tableData) {
+	($addressType,$desc) = split(/\|/,$row);
+	$json = qq[{"addressType":"$addressType","desc":"$desc"}];
+	$post = `curl -s -w '\n' -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/addresstypes`;
+	print "$post \n\n";
+}
+
