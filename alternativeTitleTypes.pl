6b20ce3133ff969656b6d389daf1bf2a96fce9ba
Guy Dobson <gdobson@drew.edu>
2021-02-26 10:57:37 -0500

    Update alternativeTitleTypes.pl

--- alternativeTitleTypes.pl
+++ alternativeTitleTypes.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-require 'getExportImportTokens.pl'; 
+require '/opt/folio/exportImportConfig/getExportImportTokens.pl'; 
 
 use JSON; 
 
@@ -9,9 +9,10 @@ print "exporting table data \n";
 $alternativeTitleTypes = `curl -s -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/alternative-title-types?query=id="*"`;
 $hash = decode_json $alternativeTitleTypes;
 for ( @{$hash->{alternativeTitleTypes}} ) {
+	$id = $_->{'id'};
 	$name = $_->{'name'};
 	$source = $_->{'source'};
-	push(@tableData,"$name|$source");
+	push(@tableData,"$id|$name|$source");
 }
 print "@tableData \n\n";
 
@@ -28,8 +29,8 @@ for ( @{$hash->{alternativeTitleTypes}} ) {
 
 print "\nimporting table data \n\n";
 foreach $row (@tableData) {
-	($name,$source) = split(/\|/,$row);
-	$json = qq[{"name":"$name","source":"$source"}];
+	($id,$name,$source) = split(/\|/,$row);
+	$json = qq[{"id":"$id","name":"$name","source":"$source"}];
 	$post = `curl -s -w '\n' -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/alternative-title-types`;
 	print "$post \n\n";
 }
