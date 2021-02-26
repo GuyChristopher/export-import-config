7275399759ccf6f718a8304fc062d74ce9f6b113
Guy Dobson <gdobson@drew.edu>
2021-02-26 13:12:16 -0500

    Update instanceTypes.pl

--- instanceTypes.pl
+++ instanceTypes.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-require 'getExportImportTokens.pl'; 
+require '/opt/folio/exportImportConfig/getExportImportTokens.pl'; 
 
 use JSON; 
 
@@ -9,10 +9,11 @@ print "exporting table data \n";
 $instanceTypes = `curl -s -X GET -G -H '$jsonHeader' -H '$exportToken' -d 'limit=1000' $exportURL/instance-types?query=id="*"`;
 $hash = decode_json $instanceTypes;
 for ( @{$hash->{instanceTypes}} ) {
+	$id = $_->{'id'};
 	$name = $_->{'name'};
 	$code = $_->{'code'};
 	$source = $_->{'source'};
-	push(@tableData,"$name|$code|$source");
+	push(@tableData,"$id|$name|$code|$source");
 }
 print "@tableData \n\n";
 
@@ -21,16 +22,17 @@ $instanceTypes = `curl -s -X GET -G -H '$jsonHeader' -H '$importToken' -d 'limit
 $hash = decode_json $instanceTypes;
 for ( @{$hash->{instanceTypes}} ) {
 	$id = $_->{'id'};
-	print "deleting $id \n";
+	$name = $_->{'name'};
+	print "deleting $name \n";
 	$delete = `curl -s -X DELETE -H '$jsonHeader' -H '$importToken' $importURL/instance-types/$id`;
 	print "$delete\n";
 }
 
 print "\nimporting table data \n\n";
 foreach $row (@tableData) {
-	($name,$code,$source) = split(/\|/,$row);
-	$json = qq[{"name":"$name","code":"$code","source":"$source"}];
-	$post = `curl -s -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/instance-types`;
+	($id,$name,$code,$source) = split(/\|/,$row);
+	$json = qq[{"id":"$id","name":"$name","code":"$code","source":"$source"}];
+	$post = `curl -s -w '\n' -X POST -H '$jsonHeader' -H '$importToken' -d '$json' $importURL/instance-types`;
 	print "$post \n\n";
 }
 
